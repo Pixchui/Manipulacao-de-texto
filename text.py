d7176b5283e01fa27843ef0120d8a68d69ae7a37
Pixchui <matheuzinhopintosilva@gmail.com>
2026-03-20 13:40:19 -0300

    Foi-se

---
+++ text.py
@@ -0,0 +1,6 @@
+with open('teste.txt', 'w', encoding='utf-8') as arquivo:
+    arquivo.write("Primeira linha.\n")
+    arquivo.write("Segunda linha.\n")
+
+with open('teste.txt', 'a', encoding='utf-8') as arquivo:
+    arquivo.write("Terceira linha.\n")
